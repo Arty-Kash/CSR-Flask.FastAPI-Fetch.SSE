fe51a59db42967f87a703e060a920646d7dbcfa0
Arty Kash <33560795+Arty-Kash@users.noreply.github.com>
2026-03-20 11:22:39 +0900

    app.py，index.html追加

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+# 1. 最初にページを表示するためのルート
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# 2. フロントエンド（JS）から呼ばれる、データを送るためのルート
+@app.route('/api/data')
+def get_data():
+    # バックエンドで生成した固定のデータ
+    data = {
+        "message": "こんにちは！これはPythonバックエンドから送られたメッセージです。",
+        "status": "success"
+    }
+    return jsonify(data)  # JSON形式でフロントに送信
+
+if __name__ == '__main__':
+    app.run(debug=True)
