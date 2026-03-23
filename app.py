54020a3c691afb005ca48b855165605920d06dbd
Arty Kash <33560795+Arty-Kash@users.noreply.github.com>
2026-03-23 14:24:47 +0900

    修正

--- app.py
+++ app.py
@@ -5,17 +5,23 @@ app = Flask(__name__)
 # 1. 最初にページを表示するためのルート
 @app.route('/')
 def index():
+    # templatesフォルダ内のindex.htmlを探してブラウザに返す
     return render_template('index.html')
 
 # 2. フロントエンド（JS）から呼ばれる、データを送るためのルート
 @app.route('/api/data')
 def get_data():
     # バックエンドで生成した固定のデータ
+    message = "Hello from Python Backend!"
+    return jsonify({"message": message})
+
+"""
     data = {
         "message": "こんにちは！これはPythonバックエンドから送られたメッセージです。あああ",
         "status": "success"
     }
     return jsonify(data)  # JSON形式でフロントに送信
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
